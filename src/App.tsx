bc72ccce8e3e27f050d1a517e638eec24fe6a5a1
Glauco Todesco <glauco.todesco@gmail.com>
2021-10-06 17:40:04 -0300

    Aula 4.27

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,74 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Table from './components/Table/Table';
+
+
+type Post = {
+  id: number
+  title: string
+  views: number
+  author: {
+    name: string
+    avatar: string
+  }
+  conversions: {
+    thousands: number
+    percentage: number
+  }
+}
 
 function App() {
+
+  const data = useMemo<Post[]>(
+    () => [
+      {
+        author: {
+          name: 'Daniel Bonifacio',
+          avatar: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRNf0vAZLggJoZxGKpfOa3EBClHkwQmmvv9Lg&usqp=CAU'
+        },
+        id: 1,
+        conversions: {
+          percentage: 64.35,
+          thousands: 607,
+        },
+        title: 'Como dobrei meu salário aprendendo somente React',
+        views: 985415
+      },
+      {
+        author: {
+          name: 'Daniel Bonifacio',
+          avatar: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRNf0vAZLggJoZxGKpfOa3EBClHkwQmmvv9Lg&usqp=CAU'
+        },
+        id: 2,
+        conversions: {
+          percentage: 64.35,
+          thousands: 607,
+        },
+        title: 'React.js vs. React Native: a REAL diferença entre os dois',
+        views: 985415
+      },
+      {
+        author: {
+          name: 'Daniel Bonifacio',
+          avatar: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRNf0vAZLggJoZxGKpfOa3EBClHkwQmmvv9Lg&usqp=CAU'
+        },
+        id: 3,
+        conversions: {
+          percentage: 95.35,
+          thousands: 845,
+        },
+        title: 'Como dobrei meu salário aprendendo somente React',
+        views: 985415
+      }
+    ],
+    []
+  )
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          <Table />
+        
       </header>
     </div>
   );
---
+++ src/stories/NoData.stories.tsx
@@ -0,0 +1,23 @@
+import { Story, Meta } from '@storybook/react';
+import NoData, { NoDataProps } from '../components/NoData/NoData';
+
+export default {
+  title: 'Components/NoData',
+  component: NoData,
+  argTypes: {
+    onNoData: { action: 'confirm' },
+    onCancel: { action: 'cancel' },
+  },
+} as Meta;
+
+const Template: Story<NoDataProps> = (args) => <NoData {...args} />;
+
+export const Default = Template.bind({});
+
+Default.args = {}
+
+export const FixedHeight = Template.bind({});
+
+FixedHeight.args = {
+  height: 240
+}
