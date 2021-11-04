8524ca56545b461a3090004775f62fb69756c2e0
Glauco Todesco <glauco.todesco@gmail.com>
2021-11-04 19:39:28 -0300

    Add CircleChart Compoment

---
+++ src/components/ImageUpload/ImageUpload.tsx
@@ -0,0 +1,14 @@
+import * as IU from './ImageUpload.sytles'
+
+export interface ImageUploadProps {
+    label: string
+  }
+  
+  function ImageUpload (props: ImageUploadProps) {
+    return <IU.Wrapper>
+          todo: image upload
+        </IU.Wrapper>
+    
+  }
+  export default ImageUpload;
+  
---
+++ src/stories/ImageUpload.stories.tsx
@@ -0,0 +1,14 @@
+import { Story, Meta } from '@storybook/react';
+import ImageUpload, { ImageUploadProps } from '../components/ImageUpload';
+
+export default {
+  title: 'Components/ImageUpdload',
+  component: ImageUpload
+} as Meta;
+
+const Template: Story<ImageUploadProps> = (args) => <ImageUpload {...args} />;
+
+export const Default = Template.bind({})
+Default.args = {
+
+}
